067b6e8d71aac0b97bf4c887b19224a537266e7b
lucrezia011 <landilucrezia.01@gmail.com>
2024-03-08 14:45:25 +0100

    esercitazione

--- src/routes/(database)/config.ts
+++ src/routes/(database)/config.ts
@@ -2,4 +2,8 @@ import { database, P } from '$cms'; // eslint-disable-line
 
 /* --- */
 
-export default database({});
+export default database ({
+    progetti: P.Object({
+    titolo: P. String (),
+    tag
+})
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,22 @@
 <script>
-	import Card from '$components/Card.svelte';
+	import { get_collection } from '$cms';
 
-	let disco_1 = {
-		artista: 'John cena',
-		disco: 'Ciao',
-		durata: '10:20',
-		anno: '1230',
-		image:
-			'https://images.unsplash.com/photo-1709632237343-4583d7f96f52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8'
-	};
+	const progetti = get_collection('progetti');
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
-</svelte:head>
+{#each progetti as progetto}
+	<div>
+		<p>{progetto.titolo}</p>
+		{#each progetto.tag as tag}
+			<p>{tag}</p>
+		{/each}
+	</div>
+{/each}
 
-<Card></Card>
+<style>
+	div {
+		background-color: red;
+		color: white;
+		margin-bottom: 10px;
+	}
+</style>
